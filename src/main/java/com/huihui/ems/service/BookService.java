d274b6fbcb50d15926e82275cb159b3c9a39b61a
QuietHuihui <huige02@outlook.com>
2022-12-25 21:15:44 +0800

    项目更名为LMS

--- src/main/java/com/huihui/ems/service/BookService.java
+++ src/main/java/com/huihui/ems/service/BookService.java
@@ -3,11 +3,12 @@ package com.huihui.ems.service;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.huihui.ems.pojo.Book;
 
 public interface BookService {
 
-	void addBook(Book book,HttpSession session);
+	void addBook(Book book,HttpSession session,MultipartFile photo,String tname);
 
 }
